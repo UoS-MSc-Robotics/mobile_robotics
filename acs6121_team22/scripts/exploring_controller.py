c6870339ef311a0e5e6689c64515595557ec3a44
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2024-04-17 14:50:31 +0100

    Update exploring_controller.py

--- acs6121_team22/scripts/exploring_controller.py
+++ acs6121_team22/scripts/exploring_controller.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 """Program to make the turtlebot3 robot explore the environment."""
 
-import math
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
@@ -19,19 +18,16 @@ class ExploringController():
 
         self.linear_speed = 0.26
         self.angular_speed = 1.82
-        self.clipping_distance = 3.5
-        self.scaling_factor = 402.0
-
-        self.magnitude = 0
-        self.target_angle = 0
-        self.laser_data = []
         self.full_laser_data = []
+
+        self.backward_threshold = 0.25
+        self.turning_threshold = 0.5
 
         # Subscribers
         rospy.Subscriber("/scan", LaserScan, self.laser_callback)
 
         # Publishers
-        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10, latch=True)
 
         # Timers
         rospy.Timer(rospy.Duration(time_period), self.controller_update)
@@ -42,33 +38,43 @@ class ExploringController():
     def move_forward(self):
         """Move the robot forward."""
         twist = Twist()
-        twist.linear.x = \
-            self.linear_speed * self.magnitude / self.scaling_factor
+        twist.linear.x = self.linear_speed
         twist.angular.z = 0
         self.pub.publish(twist)
 
     def move_backward(self):
         """Move the robot backward."""
         twist = Twist()
-        twist.linear.x = \
-            -self.linear_speed * self.magnitude / self.scaling_factor
+        twist.linear.x = -self.linear_speed
         twist.angular.z = 0
         self.pub.publish(twist)
     
-    def turn_left(self):
-        """Turn the robot left."""
+    def turn_smooth_left(self):
+        """Turn the robot smooth left."""
         twist = Twist()
-        twist.linear.x = 0
-        twist.angular.z = \
-            self.angular_speed * self.magnitude / self.scaling_factor
+        twist.linear.x = self.linear_speed / 2
+        twist.angular.z = self.angular_speed 
         self.pub.publish(twist)
 
-    def turn_right(self):
-        """Turn the robot right."""
+    def turn_smooth_right(self):
+        """Turn the robot smooth right."""
         twist = Twist()
-        twist.linear.x = 0
-        twist.angular.z = \
-            -self.angular_speed * self.magnitude / self.scaling_factor
+        twist.linear.x = self.linear_speed / 2
+        twist.angular.z = -self.angular_speed
+        self.pub.publish(twist)
+
+    def turn_hard_left(self):
+        """Turn the robot hard left."""
+        twist = Twist()
+        twist.linear.x = 0.0
+        twist.angular.z = self.angular_speed 
+        self.pub.publish(twist)
+
+    def turn_hard_right(self):
+        """Turn the robot hard right."""
+        twist = Twist()
+        twist.linear.x = 0.0
+        twist.angular.z = -self.angular_speed
         self.pub.publish(twist)
 
     def stop_moving(self):
@@ -81,54 +87,46 @@ class ExploringController():
     def laser_callback(self, msg):
         """Callback function for the laser scan."""
         self.full_laser_data = msg.ranges
-        # # clip the laser data
-        # self.laser_data = msg.ranges[0:90] + msg.ranges[270:360]
 
-        
-        # horizontal_component = 0
-        # vertical_component = 0
-
-        # for i in range(len(self.laser_data)):
-        #     if self.laser_data[i] > self.clipping_distance:
-        #         self.laser_data = list(self.laser_data)
-        #         self.laser_data[i] = self.clipping_distance
-        #         self.laser_data = tuple(self.laser_data)
-
-        #     horizontal_component += self.laser_data[i] * math.cos(math.radians(i))
-        #     vertical_component += self.laser_data[i] * math.sin(math.radians(i))
-        
-        # self.magnitude = math.sqrt(horizontal_component**2 + vertical_component**2)
-        # self.target_angle = math.degrees(math.atan2(vertical_component, horizontal_component))
-
-        # print("Magnitude: ", self.magnitude)
-        # print("Target Angle: ", self.target_angle)
+        for i in range(len(self.full_laser_data)):
+            if self.full_laser_data[i] == 0:
+                self.full_laser_data = list(self.full_laser_data)
+                self.full_laser_data[i] = self.clipping_distance
+                self.full_laser_data = tuple(self.full_laser_data)
 
     def obstacle_avoider(self):
         """Avoid obstacles."""
-        # if 85 < self.target_angle < 95:
-        #     self.move_forward()
-        # if self.target_angle < 85:
-        #     self.turn_left()
-        # if self.target_angle > 95:
-        #     self.turn_right()
 
         # if object is headon, move backward
-        if any((distance < 0.2 for distance in self.laser_data[0:20]) or any(distance < 0.2 for distance in self.laser_data[70:90])):
+        if any((distance < self.backward_threshold for distance in self.full_laser_data[0:20]) or any(distance < self.backward_threshold for distance in self.full_laser_data[70:90])):
             self.move_backward()
-        # if there is significant obstacles in the indices from 0 to 65, turn right
-        elif any(distance < 0.5 for distance in self.laser_data[0:65]):
-            self.turn_right()
-        # if there is significant obstacles in the indices from 295 to 360, turn left
-        elif any(distance < 0.5 for distance in self.laser_data[295:360]):
-            self.turn_left()
+            print("backward")
+        # if there is significant obstacles in the indices from 0 to 40, turn right
+        elif any(distance < self.turning_threshold for distance in self.full_laser_data[0:40]):
+            if any(distance < self.turning_threshold/2 for distance in self.full_laser_data[0:40]):
+                self.turn_hard_right()
+                print("hard right")
+            else:
+                print("smooth right")
+                self.turn_smooth_right()
+        # if there is significant obstacles in the indices from 320 to 360, turn left
+        elif any(distance < self.turning_threshold for distance in self.full_laser_data[320:360]):
+            if any(distance < self.turning_threshold/2 for distance in self.full_laser_data[320:360]):
+                self.turn_hard_left()
+                print("hard left")
+            else:
+                print("smooth left")
+                self.turn_smooth_left()
         else:
             self.move_forward()
+            print("forward")
 
     def controller_update(self, _):
         """Update the controller."""
         # Check if the laser data is available
-        if self.laser_data:
+        if self.full_laser_data:
             self.obstacle_avoider()
+            pass
         else:
             self.stop_moving()
 
